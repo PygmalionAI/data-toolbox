7515bfc84ae9e805854bd012028e1997cdfddf00
0x000011b <0x000011b@waifu.club>
2023-02-06 17:56:38 -0300

    chore: delete unused constant

--- toolbox/core/consts.py
+++ toolbox/core/consts.py
@@ -17,11 +17,6 @@ class PromptConstants:
     # Token to separate prompt trickery from actual dialogue.
     CHAT_START_TOKEN = "<START>"
 
-    # Global target word count. The word count is chosen in such a way that we
-    # can fit all the required prompt trickery into the model's input, but still
-    # leave enough space for the user's input message and the inference result.
-    TARGET_WORD_COUNT_PER_EPISODE = 1024
-
     @staticmethod
     def pdm_prefix_for(name: str) -> str:
         '''Builds the Persona Dialogue Module prefix for a given `name`.'''
