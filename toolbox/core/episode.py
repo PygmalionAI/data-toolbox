f5e08e37abc6f3abf2b48ae967197ec65ec7f0bb
0x000011b <0x000011b@waifu.club>
2023-02-05 21:36:49 -0300

    fix: SFT example generator

--- toolbox/core/episode.py
+++ toolbox/core/episode.py
@@ -25,17 +25,20 @@ class SupervisedEpisodeProcessor:
             HuggingFace's `from_pretrained()`
         :param target_length: Usually, the model's maximum context size.
         '''
-        self.tokenizer = tokenizers.Tokenizer.from_pretrained(tokenizer_name)
+        self.tokenizer: tokenizers.Tokenizer = tokenizers.Tokenizer.from_pretrained(
+            tokenizer_name)
         self.target_length = target_length
 
         super().__init__()
 
-    def process(self,
-                episode: Episode) -> t.Generator[SupervisedExample, None, None]:
+    def process(
+        self, episode: Episode
+    ) -> t.Generator[t.Tuple[Episode, SupervisedExample], None, None]:
         # Start off with the persona data at the top.
         base_prompt = ""
         for speaker, persona in episode.participant_personas.items():
-            base_prompt += f"{speaker}'s Persona: {persona}\n"
+            pdm_prefix = PromptConstants.pdm_prefix_for(speaker)
+            base_prompt += f"{pdm_prefix}: {persona}\n"
 
         # Then, world scenario.
         if episode.world_scenario:
@@ -53,31 +56,52 @@ class SupervisedEpisodeProcessor:
 
         # Afterwards, we start dealing with chat history that can be broken
         # apart into separate training examples.
-        cur_prompt = base_prompt
         cur_len = base_len
         cur_turns: list[Turn] = []
 
         for turn in episode.turns:
-            last_turn = cur_turns[-1]
+            last_turn = cur_turns[-1] if len(cur_turns) > 1 else None
 
             # If we have enough turns and the last one is not from a human, we
             # can yield a training example.
-            if len(cur_turns) > 1 and not last_turn.human_speaker:
-                # Collapse `cur_turns` down into text and append to `cur_prompt`
-                cur_prompt += "\n".join(
+            if last_turn and not last_turn.human_speaker:
+                # Collapse `cur_turns` down into text.
+                prompt = base_prompt
+                prompt += "\n".join(
                     [f"{t.speaker}: {t.utterance}" for t in cur_turns[:-1]])
 
                 # Append response prefix into `cur_prompt`, and yield the
                 # example.
-                cur_prompt += f"{last_turn.speaker}:"
-                yield SupervisedExample(prompt=cur_prompt,
-                                        response=last_turn.utterance)
+                prompt += f"\n{last_turn.speaker}:"
+                trimmed_episode = Episode(
+                    turns=cur_turns,
+                    participant_personas=episode.participant_personas,
+                    world_scenario=episode.world_scenario,
+                )
+                example = SupervisedExample(prompt=prompt,
+                                            response=last_turn.utterance)
+
+                # NOTE: Needing to yield both the trimmed episode _and_ the
+                # formatted training example is a side-effect of bad modeling on
+                # my end. The filters expect episodes, but if we apply them on
+                # the non-trimmed episodes we lose out on way too much data, so
+                # we yield the trimmed version so we can run _those_ through the
+                # filters instead.
+                yield trimmed_episode, example
+
+                # Sanity check so I can catch this easier in case I break
+                # something.
+                example_len = self._tokenized_length(prompt +
+                                                     last_turn.utterance)
+                if example_len > self.target_length:
+                    LOG.error(
+                        f"Generated an example too large ({example_len} > {self.target_length})"
+                    )
 
             if cur_len + self._turn_length(turn) > self.target_length:
                 # Can't add this turn into this context window. Take what we
                 # already have and yield it, then add this turn to the next
                 # window.
-                cur_prompt = base_prompt
                 cur_len = base_len + self._turn_length(turn)
                 cur_turns = [turn]
             else:
@@ -91,4 +115,4 @@ class SupervisedEpisodeProcessor:
 
     def _tokenized_length(self, string: str) -> int:
         '''Returns the length of the given `string`, in tokens.'''
-        return len(self.tokenizer(string).input_ids)
+        return len(self.tokenizer.encode(string))
