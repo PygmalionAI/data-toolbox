554330cc35000daa7e496016d387c121bb5e12ec
TearGosling <teargoslingwaifu@gmail.com>
2023-07-20 17:16:35 -0500

    BEST DEVELOPER 2023
    Jesus H.

--- toolbox/tasks/__init__.py
+++ toolbox/tasks/__init__.py
@@ -1,7 +1,7 @@
 import typing as t
 
 from toolbox.core.task import BaseTask
-from toolbox.tasks.airoboros_instruction_following import Airoboros
+from toolbox.tasks.airoboros_instruction_following import AiroborosInstructionFollowingTask
 from toolbox.tasks.aidungeon_text_adventure import AiDungeonTextAdventureTask
 from toolbox.tasks.characterai_roleplay import CharacterAiRoleplayTask
 from toolbox.tasks.clubfloyd_text_adventure import ClubFloydTextAdventureTask
@@ -22,6 +22,7 @@ from toolbox.tasks.wizard_vicuna_question_answering import \
 
 NAME_TO_TASK_MAPPING: dict[str, t.Type[BaseTask]] = {
     cls.__name__: cls for cls in [
+        AiroborosInstructionFollowingTask,
         AiDungeonTextAdventureTask,
         CharacterAiRoleplayTask,
         ClubFloydTextAdventureTask,
