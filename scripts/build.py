4e8ac09c4b879667652617f1267f54c75ccba735
TearGosling <teargoslingwaifu@gmail.com>
2023-08-14 20:02:34 -0500

    feat: Add max length arg
    Honestly, why didn't I do this much earlier?

--- scripts/build.py
+++ scripts/build.py
@@ -6,6 +6,7 @@ import json
 
 from colors import color
 
+from toolbox.core.task import BaseTask
 from toolbox.core.training_example import TrainingExampleGenerator, TurnTooLargeError
 from toolbox.filters.training_example_filter import TrainingExampleFilter
 from toolbox.tasks import NAME_TO_TASK_MAPPING
@@ -15,72 +16,84 @@ LOG = logging.getLogger(__name__)
 
 
 def main() -> None:
-    random.seed(42)
-
     args = _parse_args_from_argv()
     logging.basicConfig(
         format='[%(asctime)s] [%(levelname)s] %(message)s',
         level=logging.DEBUG if args.verbose else logging.INFO,
     )
 
+    random.seed(args.seed)
+
+    if not args.print and args.output_file.strip() == "":
+        raise ValueError("Invalid directory specified! Did you mean to enable the `print` flag?")
+
     idx = 0
     print_new_episode_header = True
 
+    # Generate tasks and example filters
+    tasks: list[BaseTask] = [NAME_TO_TASK_MAPPING[task]() for task in args.tasks.split(",")]
     example_filters: list[TrainingExampleFilter] = [
         NAME_TO_TRAINING_EXAMPLE_FILTER_MAPPING[filter_name]()
         for filter_name in args.example_filters.split(",")
-    ] if args.example_filters else []
+    ] if args.filters else []
 
-    Task = NAME_TO_TASK_MAPPING[args.task]
-    for episode in Task():
-        if args.print and print_new_episode_header:
-            print(
-                color("     new episode      ",
-                      fg="black",
-                      bg="green",
-                      style="bold"))
-            print_new_episode_header = False
+    if not args.print:
+        f = open(args.output_file, "w", encoding="utf-8")
 
-        try:
-            for example in TrainingExampleGenerator(episode):
-                # Right off the bat, if this training example gets caught by one
-                # of the filters, skip over and don't even count it.
-                should_keep = True
-                for filter in example_filters:
-                    if not filter.should_keep(example):
-                        should_keep = False
-                        break
-                if not should_keep:
-                    continue
+    for task in tasks:
+        for episode in task:
+            if args.print and print_new_episode_header:
+                print(
+                    color("     new episode      ",
+                        fg="black",
+                        bg="green",
+                        style="bold")
+                )
+                print_new_episode_header = False
+            
+            try:
+                for example in TrainingExampleGenerator(episode, target_token_count=args.max_length):
+                    # Right off the bat, if this training example gets caught by one
+                    # of the filters, skip over and don't even count it.
+                    should_keep = True
+                    for filter in example_filters:
+                        if not filter.should_keep(example):
+                            should_keep = False
+                            break
+                    if not should_keep:
+                        continue
 
-                idx += 1
-                if idx < args.starting_index:
-                    continue
-                if args.max_count and (idx >
-                                       args.starting_index + args.max_count):
-                    quit()
+                    idx += 1
+                    if idx < args.starting_index:
+                        continue
+                    if args.max_count and (idx >
+                                        args.starting_index + args.max_count):
+                        quit()
 
-                print_new_episode_header = True
-                if args.print:
-                    print(
-                        color("   training example   ",
-                              fg="black",
-                              bg="orange",
-                              style="bold"))
+                    print_new_episode_header = True
 
-                    print(color(example.prompt, fg="gray"), end="")
-                    print(color(example.generation, fg="green"))
-                else:
-                    print(
-                        json.dumps({
+                    if args.print:
+                        print(
+                            color("   training example   ",
+                                fg="black",
+                                bg="orange",
+                                style="bold")
+                        )
+                        print(color(example.prompt, fg="gray"), end="")
+                        print(color(example.generation, fg="green"))
+                    else:
+                        dict_to_write = {
                             "prompt": example.prompt,
                             "generation": example.generation,
                             "identifier": example.identifier,
-                        }))
-        except TurnTooLargeError:
-            LOG.info("Skipping over episode (%s) due to a TurnTooLargeError",
-                     episode.identifier)
-
+                        }
+                        f.write(json.dumps(dict_to_write) + "\n")
+            except TurnTooLargeError:
+                LOG.info("Skipping over episode (%s) due to a TurnTooLargeError",
+                        episode.identifier)
+                
+    if not args.print:
+        f.close()
 
 #
 # Helpers and CLI entrypoint.
@@ -90,41 +103,72 @@ def main() -> None:
 def _parse_args_from_argv() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-t",
-                        "--task",
-                        required=True,
-                        help="Which task to build data for.")
-                        
-    parser.add_argument("-o",
-                        "--output-file",
-                        default="",
-                        help="The path to the output file of the built data")
+    parser.add_argument(
+        "-t",
+        "--tasks",
+        type=str,
+        required=True,
+        help="The tasks to build data for, comma-separated."
+    )
 
     parser.add_argument(
-        "--example-filters",
+        "-o",
+        "--output-file",
         type=str,
-        help="List of comma-separated filters to apply to training examples.")
+        default="", # Not required if examples just need to be printed
+        help="The tasks to build data for, comma-separated."
+    )
 
-    parser.add_argument("--starting-index",
-                        type=int,
-                        default=0,
-                        help="Used to skip over training examples.")
+    parser.add_argument(
+        "-f",
+        "--filters",
+        type=str,
+        help="List of comma-separated filters to apply to training examples."
+    )
 
-    parser.add_argument("--max-count",
-                        type=int,
-                        default=None,
-                        help="Limit how many training examples to generate.")
+    parser.add_argument(
+        "-l",
+        "--max-length",
+        type=int,
+        default=2048,
+        # TODO(TG): Explain this more clearly
+        help="The (approximate) amount of tokens to limit episodes to."
+    )
 
     parser.add_argument(
         "-p",
         "--print",
         action="store_true",
-        help="Print training examples instead of writing to STDOUT.")
+        help="Print training examples instead of writing to STDOUT."
+    )
 
-    parser.add_argument("-v",
-                        "--verbose",
-                        action="store_true",
-                        help="Enable verbose logging.")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Enable verbose logging."
+    )
+
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="The seed for the random number generator."
+    )
+
+    parser.add_argument(
+        "--starting-index",
+        type=int,
+        default=0,
+        help="Used to skip over training examples."
+    )
+
+    parser.add_argument(
+        "--max-count",
+        type=int,
+        default=None,
+        help="Limit how many training examples to generate."
+    )
 
     return parser.parse_args()
 
