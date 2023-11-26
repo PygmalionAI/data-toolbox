04a9b59565b4f9a5d4e3e549475e5b685c9da8b1
TearGosling <teargoslingwaifu@gmail.com>
2023-11-26 14:45:59 -0600

    Basic process utils file
    Will add more to this later

---
+++ toolbox/utils/process.py
@@ -0,0 +1,43 @@
+'''Common functions for processing text.'''
+import re
+
+import ftfy
+
+# Custom ftfy config to keep stuff such as full-width text (for fun!)
+config = ftfy.TextFixerConfig(
+    fix_latin_ligatures=False,
+    fix_character_width=False,
+    # We don't need explanations for now and extra speed is wanted.
+    explain=False
+)
+
+def fix_style_and_encoding_issues(original_message: str) -> str:
+    '''Cleans up any style-related issues.'''
+    message = original_message
+    message = message.replace(" .. ", "... ")
+    message = message.replace(" ... ", "... ")
+    message = re.sub(r'\b(\.\.\.?)\b', '... ', message)
+
+    message = message.replace(" . ", ". ")
+    message = message.replace(" , ", ", ")
+    message = message.replace(" ? ", "? ")
+    message = message.replace(" ! ", "! ")
+
+    message = re.sub(r"(\S)(…)(\S)", "\\1\\2 \\3", message)
+
+    # Fix hidden zero-width spaces and other whitespace fuckery which could
+    # mess with a model.
+    document = document.replace(" ", "")
+    document = document.replace("​", "")
+    document = document.replace("‍", " ")
+    document = document.replace(" ", " ")
+    document = document.replace("﻿", " ")
+    document = document.replace("", "")
+    document = document.replace("‎", "")
+
+    # Some forums have their pages incorrectly tagged as UTF-8, so we get
+    # garbage when decoding. Most common problem I've seen is bad quotation
+    # marks, so we paper over that here.
+    message = ftfy.fix_text(message, config=config)
+
+    return message
