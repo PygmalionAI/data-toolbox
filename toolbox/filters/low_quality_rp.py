1e824b99b2c47abe0e64ba386c30f5f87d88dd3c
TearGosling <teargoslingwaifu@gmail.com>
2023-12-06 01:28:18 -0600

    Make LowQualityRpFilter use a threshold
    Dropping entire examples because one (1) turn out of dozens had a floating quotation mark in its text is kinda stupid. Better to use a threshold.

--- toolbox/filters/low_quality_rp.py
+++ toolbox/filters/low_quality_rp.py
@@ -5,6 +5,8 @@ from ..core import BaseFilter, Episode, TurnKind
 
 LOG = logging.getLogger("LowQualityRpFilter")
 
+THRESHOLD = 0.5
+
 # Regex patterns. They search for the following:
 # -----
 # "Floating" quotation marks.
@@ -22,13 +24,13 @@ MUSHED_QUOTATION_MARKS_PATTERN = re.compile(r'\S"\S')
 # Put this in one big dictionary so we don't have to copy-paste code.
 # The keys correspond to the pattern and the value corresponds to what will
 # be logged.
-PATTERNS: dict[re.Pattern, str] = {
-    FLOATING_QUOTATIONS_PATTERN: "floating quotation marks",
-    MUSHED_QUOTATION_MARKS_PATTERN: "mushed quotation marks",
-    MUSHED_PARENTHESIS_PATTERN: "mushed parenthesis",
-    LOWERCASE_I_PATTERN: "potential bad-quality writing",
-    LINKS_PATTERN: "possibly containing links",
-}
+PATTERNS: list[re.Pattern] = [
+    FLOATING_QUOTATIONS_PATTERN,
+    MUSHED_QUOTATION_MARKS_PATTERN,
+    MUSHED_PARENTHESIS_PATTERN,
+    LOWERCASE_I_PATTERN,
+    LINKS_PATTERN,
+]
 
 class LowQualityRpFilter(BaseFilter):
     '''
@@ -39,13 +41,24 @@ class LowQualityRpFilter(BaseFilter):
         pass
 
     def should_keep(self, episode: Episode) -> bool:
-        for i, turn in enumerate(episode.turns, start=1):
-            # Ignore the system prompt on this one.
+        low_quality_messages = 0
+        for turn in episode.turns:
             message = turn.utterance
+            # Ignore the system prompt on this one.
             if turn.kind != TurnKind.SYSTEM:
-                # "Floating" quotation marks
-                for pattern, msg in PATTERNS.items():
+                # Go through RegEx patterns.
+                for pattern in PATTERNS:
                     if pattern.search(message) is not None:
-                        LOG.info(f"Episode {episode.identifier} dropped due to {msg} in turn {i}!")
-                        return False
+                        low_quality_messages += 1
+                        break
+                # If the message is empty, we count it as a low-quality message.
+                if message.strip() == "":
+                    low_quality_messages += 1
+        
+        # If the ratio of low-quality messages is greater than the threshold,
+        # drop the episode.
+        if low_quality_messages / len(episode.turns) >= THRESHOLD:
+            LOG.info(f"Episode {episode.identifier} dropped due to signs of low-quality RP!")
+            return False
+        
         return True
