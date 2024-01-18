1fdf6a8bb1e21e17baa497bc275b8521285d81ab
TearGosling <teargoslingwaifu@gmail.com>
2024-01-18 10:04:51 -0600

    Add support for different Aesir files

--- toolbox/datasets/aesir.py
+++ toolbox/datasets/aesir.py
@@ -7,7 +7,7 @@ import ujson
 
 from .common import MessageAndRole, MessageAndRoleConversation
 from ..core import BaseDataset
-from ..utils import get_path_for
+from ..utils import enumerate_files_for, get_path_for
 
 LOG = logging.getLogger(__name__)
 
@@ -17,17 +17,21 @@ class AesirDataset(BaseDataset[MessageAndRole]):
     '''
     def __iter__(self) -> Generator[MessageAndRoleConversation, None, None]:
         root_path = get_path_for("aesir")
-        file_path = os.path.join(root_path, "aesir-rpg-charcards.json")
+        # Handle the GPT-3.5 data as a json, if it exists
+        gpt3_file_path = os.path.join(root_path, "aesir-rpg-charcards.json")
 
-        with open(file_path, "r", encoding="utf-8") as f:
-            # Load the file.
-            data = ujson.load(f)
-            # Iterate through the data.
-            for conv in data:
-                convo = [
-                    MessageAndRole(
-                        message=e["value"],
-                        role=e["from"]
-                    ) for e in conv["conversations"]
-                ]
-                yield MessageAndRoleConversation(conversation=convo)
+        if os.path.exists(gpt3_file_path):
+            with open(gpt3_file_path, "r", encoding="utf-8") as f:
+                # Load the file.
+                data = ujson.load(f)
+                # Iterate through the data.
+                for conv in data:
+                    convo = [
+                        MessageAndRole(
+                            message=e["value"],
+                            role=e["from"]
+                        ) for e in conv["conversations"]
+                    ]
+                    yield MessageAndRoleConversation(conversation=convo)
+
+        
